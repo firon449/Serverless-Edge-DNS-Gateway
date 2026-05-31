79a6d4ad1b1d2200a6fbd6faced924d44772fdb8
firon449 <doangoctung@gmail.com>
2026-05-31 17:14:17 +0700

    Update update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -1,55 +1,55 @@
-#!/bin/bash
-
-# Làm rỗng danh sách chặn quảng cáo hoàn toàn
-echo -n "" > rules/blocklists.txt
-
-# Nếu bạn vẫn muốn giữ bộ lọc allowlists.txt thì để nguyên phần dưới, 
-# hoặc làm rỗng luôn tương tự:
-echo -n "" > rules/allowlists.txt
-
 # #!/bin/bash
 
-# # Định nghĩa đường dẫn tương đối trong Github Workspace
-# DIR="rules"
-# BLOCK_OUT="./$DIR/blocklists.txt"
-# ALLOW_OUT="./$DIR/allowlists.txt"
-# BLOCK_TMP="/tmp/blocklists.tmp"
-# ALLOW_TMP="/tmp/allowlists.tmp"
+# # Làm rỗng danh sách chặn quảng cáo hoàn toàn
+# echo -n "" > rules/blocklists.txt
 
-# # Tạo thư mục rules nếu chưa có
-# mkdir -p "./$DIR"
+# # Nếu bạn vẫn muốn giữ bộ lọc allowlists.txt thì để nguyên phần dưới, 
+# # hoặc làm rỗng luôn tương tự:
+# echo -n "" > rules/allowlists.txt
 
-# # Cleanup khi script exit
-# trap "rm -f $BLOCK_TMP $ALLOW_TMP; exit" INT TERM EXIT
+#!/bin/bash
 
-# extract_domains() {
-#   awk '{
-#     if (/^[[:space:]]*$/ || /^[!#]/) next
-#     line = tolower($0)
-#     sub(/^@@\|\|?/, "", line)
-#     sub(/^\|\|?/, "", line)
-#     sub(/\^.*/, "", line)
-#     sub(/[#!].*/, "", line)
-#     sub(/\/.*/, "", line)
-#     sub(/:.*/, "", line)
-#     sub(/^[0-9.]+[[:space:]]+/, "", line)
-#     gsub(/^[[:space:]]+|[[:space:]]+$/, "", line)
-#     if (line ~ /^[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?(\.[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?)+$/ && !seen[line]++) print line
-#   }'
-# }
+# Định nghĩa đường dẫn tương đối trong Github Workspace
+DIR="rules"
+BLOCK_OUT="./$DIR/blocklists.txt"
+ALLOW_OUT="./$DIR/allowlists.txt"
+BLOCK_TMP="/tmp/blocklists.tmp"
+ALLOW_TMP="/tmp/allowlists.tmp"
 
-# echo "Downloading and processing blocklists..."
-# curl -fsSL --max-time 60 \
-# https://raw.githubusercontent.com/bibicadotnet/blocklist_minimal/main/blocklists.txt \
-# | extract_domains > "$BLOCK_TMP"
+# Tạo thư mục rules nếu chưa có
+mkdir -p "./$DIR"
 
-# echo "Downloading and processing allowlists..."
-# curl -fsSL --max-time 60 \
-# https://raw.githubusercontent.com/bibicadotnet/AdGuard-Home-blocklists/refs/heads/main/whitelist.txt \
-# | extract_domains > "$ALLOW_TMP"
+# Cleanup khi script exit
+trap "rm -f $BLOCK_TMP $ALLOW_TMP; exit" INT TERM EXIT
 
-# # Di chuyển file tmp vào thư mục đích
-# mv "$BLOCK_TMP" "$BLOCK_OUT"
-# mv "$ALLOW_TMP" "$ALLOW_OUT"
+extract_domains() {
+  awk '{
+    if (/^[[:space:]]*$/ || /^[!#]/) next
+    line = tolower($0)
+    sub(/^@@\|\|?/, "", line)
+    sub(/^\|\|?/, "", line)
+    sub(/\^.*/, "", line)
+    sub(/[#!].*/, "", line)
+    sub(/\/.*/, "", line)
+    sub(/:.*/, "", line)
+    sub(/^[0-9.]+[[:space:]]+/, "", line)
+    gsub(/^[[:space:]]+|[[:space:]]+$/, "", line)
+    if (line ~ /^[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?(\.[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?)+$/ && !seen[line]++) print line
+  }'
+}
 
-# echo "Done. Files saved to $BLOCK_OUT and $ALLOW_OUT"
+echo "Downloading and processing blocklists..."
+curl -fsSL --max-time 60 \
+https://raw.githubusercontent.com/bibicadotnet/blocklist_minimal/main/blocklists.txt \
+| extract_domains > "$BLOCK_TMP"
+
+echo "Downloading and processing allowlists..."
+curl -fsSL --max-time 60 \
+https://raw.githubusercontent.com/bibicadotnet/AdGuard-Home-blocklists/refs/heads/main/whitelist.txt \
+| extract_domains > "$ALLOW_TMP"
+
+# Di chuyển file tmp vào thư mục đích
+mv "$BLOCK_TMP" "$BLOCK_OUT"
+mv "$ALLOW_TMP" "$ALLOW_OUT"
+
+echo "Done. Files saved to $BLOCK_OUT and $ALLOW_OUT"
